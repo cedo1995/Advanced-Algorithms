181d0a6d4d49d94a37d709da2b051179793420a8
Cedo <roberto.cedo@gmail.com>
2019-04-09 16:00:19 +0200

    Consegna RA con codice commentato

--- Lab2/BinaryHeap.py
+++ Lab2/BinaryHeap.py
@@ -5,31 +5,47 @@ import sys
 class Tuple:
     def __init__(self, id, value):
         self.id = id  #  id della stazione a cui corrisponde il nodo
-        self.value = value
+        self.value = value  # valore del nodo nella heap che corrisponde alla distanza dall origine
 
 class BinaryHeap:
     def __init__(self):
         self.list_vertices = []  # lista di nodi contenuti nella heap binaria ogni elemento è una Tupla (valore, id stazione)
 
     def add(self, id, value):
+        """
+        :param id: id nodo
+        :param value: valore del nodo che corrisponde alla distanza
+        :return: aggiunge alla heap il nodo
+        """
         self.list_vertices.append(Tuple(id, value))
         n = len(self.list_vertices)
         self.bubbleUp(n-1)
 
-    def buildHeap(self, n):
-        for i in range(int(n/2)-1, 0, -1):
-            self.trickleDown(i)
-
     def left(self, i):
+        """
+        :param i: indice nodo
+        :return: ritorna il nodo a sinistra del nodo di indice i
+        """
         return (2*i)+1
 
     def right(self, i):
+        """
+        :param i: indice nodo
+        :return: ritorna il nodo a destra del nodo di indice i
+        """
         return (2*i)+2
 
     def parent(self, i):
+        """
+        :param i: indice del nodo
+        :return: ritorna il padre del nodo di indice i
+        """
         return int((i-1)/2)
 
-    def bubbleUp(self, i):
+    def bubbleUp(self, i):  # modifica la heap facendo salire nella posizione corretta il nodo di indice i
+        """
+        :param i: indice del nodo
+        """
         p = self.parent(i)
         while i > 0 and self.list_vertices[i].value < self.list_vertices[p].value:
             temp = self.list_vertices[p]
@@ -38,11 +54,10 @@ class BinaryHeap:
             i = p
             p = self.parent(i)
 
-    def decreaseKey(self, nodeId, value):
+    def decreaseKey(self, nodeId, value):  # diminuisce il valore del nodo al valore value
         """
-        :param nodeId: NON è un IdToNumber
+        :param nodeId: id del nodo
         :param value: è la distanza dallo starting node al nodo con id = nodeId
-        :return:
         """
         for i, x in enumerate(self.list_vertices):
             if x.id == nodeId and self.list_vertices[i].value > value:
@@ -51,7 +66,10 @@ class BinaryHeap:
                 return True
         return False
 
-    def trickleDown(self, i):
+    def trickleDown(self, i):  # fa scendere un nodo nella heap
+        """
+        :param i: id del nodo
+        """
         l = self.left(i)
         r = self.right(i)
         n = len(self.list_vertices)
@@ -67,6 +85,9 @@ class BinaryHeap:
             self.trickleDown(smallest)
 
     def extractMin(self):
+        """
+        :return: ritorna il primo nodo nella heap che sarà quello con valore (distanza) minore
+        """
         minimum = self.list_vertices[0]
         n = len(self.list_vertices)
         self.list_vertices[0] = self.list_vertices[n - 1]
