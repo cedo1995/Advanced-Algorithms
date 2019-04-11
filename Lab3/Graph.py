7dc6e3c2fc46f27fc9ad1343f0c977365316e917
Cedo <roberto.cedo@gmail.com>
2019-04-11 20:34:20 +0200

    intestazione main modificata, aggiunti i pesi delle coordinate ai grafi

--- Lab3/Graph.py
+++ Lab3/Graph.py
@@ -1,7 +1,7 @@
-
+import math
 
 class Graph:
-    def __init__(self, name, dimension, coord_type, coordinates, count_to_coordinates):
+    def __init__(self, name, dimension, coord_type, coordinates):
         """
         :param name: nome del grafo che si sta analizzando
         :param dimension: numero dei nodi del grafo
@@ -13,17 +13,33 @@ class Graph:
         self.num_nodes = int(dimension)
         self.coord_type = coord_type
         self.coordinates = coordinates
-        self.count_to_coordinates = count_to_coordinates        # dizionario indice--> coordinate
         self.num_edges = 0          # numero di archi del grafo
         self.edges = []             # archi del grafo
+        self.weights = []
 
         # aggiunta di tutti gli archi al grafo in modo da creare un grafo completo
         for i in range(self.num_nodes-1):
             for j in range(i+1, self.num_nodes):
                 self.num_edges += 1
                 self.edges.append((self.coordinates[i], self.coordinates[j]))
+                self.weights.append(self.calculateWeight(self.coordinates[i], self.coordinates[j]))
+
 
 
 
     def printG(self):
         print("Il grafo", self.name, "ha ", self.num_nodes, "nodi e ", self.num_edges, "archi")
+
+
+    def calculateWeight(self, coordinates1, coordinates2):
+        if self.coord_type == "GEO\n":
+            x = abs(coordinates1[0] - coordinates2[0])
+            y = abs(coordinates1[1] - coordinates2[1])
+            return round((x**2 + y**2)**0.5)
+        else:
+            RRR = 6378.388
+
+            q1 = math.cos(coordinates1[1] - coordinates2[1])
+            q2 = math.cos(coordinates1[0] - coordinates2[0])
+            q3 = math.cos(coordinates1[0] + coordinates2[0])
+            return round(RRR * math.acos(0.5 * ((1.0 + q1) * q2 - (1.0 - q1) * q3)) + 1.0)
--- Lab3/Main.py
+++ Lab3/Main.py
@@ -1,11 +1,11 @@
-import fileinput
-import errno
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
 import codecs
 from Graph import Graph
+import math
 import sys
 
 def main():
-    # sys.setrecursionlimit(sys.maxsize)
     path_info = ["./Files/burma14.tsp", "./Files/d493.tsp", "./Files/dsj1000.tsp", "./Files/eil51.tsp",
                  "./Files/gr229.tsp", "./Files/kroD100.tsp", "./Files/ulysses22.tsp"]
     for i in range(len(path_info)):
@@ -13,8 +13,6 @@ def main():
         dimension = -1      # numero dei nodi
         coord_type = ""     # tipo di coordinate
         coordinates = []    # lista di tuple della forma (coordinata1, coordinata2)
-        count = 1           # contatore dei nodi
-        count_to_coordinates = {}       # dizionario che associa il numero del nodo alle rispettive coordinate
         next_are_coords = False     # se è True significa che sto parsando le coordinate
         line = ""       # inizializzazione della linea che verrà utilizzata per scorrere il file
 
@@ -25,29 +23,36 @@ def main():
 
                     a = line.split(":")     # divido la linea rispetto al carattere :
                     if a[0] == "NAME" or a[0] == "NAME ":
-                        name = a[1].split(" ")[0]   # salvo il nome del grafo
+                        name = a[1].replace(" ", "")[:-1]   # salvo il nome del grafo
+
                     if a[0] == "DIMENSION" or a[0] == "DIMENSION ":
-                        dimension = int(a[1].split(" ")[1])     # salvo la dimensione dei nodi del grafo
+                        dimension = int(a[1].replace(" ", ""))     # salvo la dimensione dei nodi del grafo
                     if a[0] == "EDGE_WEIGHT_TYPE" or a[0] == "EDGE_WEIGHT_TYPE ":
-                        coord_type = a[1].split(" ")[0]     # salvo il tipo di coordinate
+                        coord_type = a[1].replace(" ", "")   # salvo il tipo di coordinate
                     if a[0] == "NODE_COORD_SECTION\n":
                         next_are_coords = True      # le prossime righe del file saranno coordinate
 
                 else:
                     a = line.split(" ")     # divido la linea rispetto agli spazi
+                    a = [i for i in a if i != ""]
                     if coord_type == "EUC_2D":      # se sono euclidee
-                        coordinates.append((float(a[1]), float(a[2])))      # float gestisce la codifica numerica esponenziale
-                        count_to_coordinates[count] = coordinates[-1]       # indice della coordinata--> coordinate
+                        coordinates.append((float(a[1]), float(a[2][:-1])))      # float gestisce la codifica numerica esponenziale
 
                     else:
-                        coordinates.append((a[1], a[2]))    # aggiungo le coordinate alla lista
-                        count_to_coordinates[count] = coordinates[-1]       # indice della coordinata--> coordinate
+                        deg = int(float(a[1]))
+                        print(deg)
+                        min = float(a[1]) - deg
+                        radX = math.pi * (deg + 5.0 * min / 3.0) / 180.0
 
-                    count += 1          # incremento l'indice
+                        deg = int(float(a[2][:-1]))
+                        min = float(a[2][:-1]) - deg
+                        radY = math.pi * (deg + 5.0 * min / 3.0) / 180.0
+
+                        coordinates.append((radX, radY))    # aggiungo le coordinate alla lista
 
                 line = f.readline()     # passo alla linea successiva
 
-            graph = Graph(name, dimension, coord_type, coordinates, count_to_coordinates)   # todo da decidere se tenere count_to_coord qui o crearlo in Graph
+            graph = Graph(name, dimension, coord_type, coordinates)   # todo da decidere se tenere count_to_coord qui o crearlo in Graph
             graph.printG()
 
 if __name__ == '__main__':
