10f3b506079bee9094f3796d27e785f36c78058d
Cedo <roberto.cedo@gmail.com>
2019-05-04 11:54:06 +0200

    iniziato a commentare i file, terminata la relazione per il laboratorio 3

--- Lab3/Graph.py
+++ Lab3/Graph.py
@@ -9,6 +9,7 @@ from Node import Node
 from Edge import Edge
 sys.setrecursionlimit(10000)
 
+
 class Graph:
     def __init__(self, name, dimension, coord_type, coordinates):
         """
@@ -16,38 +17,36 @@ class Graph:
         :param dimension: numero dei nodi del grafo
         :param coord_type: tipo di coordinate utilizzate
         :param coordinates: lista di tutte le coordinate
-        :param count_to_coordinates: dizionario che relaziona la posizione della coordinata nel file rispetto ai valori
         """
         self.name = name
         self.num_nodes = int(dimension)
         self.coord_type = coord_type
         self.coordinates = coordinates
         self.num_edges = 0          # numero di archi del grafo
-        self.matr_adj = np.zeros(shape=(self.num_nodes, self.num_nodes))
+        self.matr_adj = np.zeros(shape=(self.num_nodes, self.num_nodes))        # matrice delle adiacenze del grafo
 
-        # aggiunta di tutti gli archi al grafo in modo da creare un grafo completo
-        for i in range(self.num_nodes-1):
+        # aggiungo tutti gli archi al grafo in modo da creare un grafo completo
+        for i in range(self.num_nodes-1):       # scorro solo la matrice triangolare superiore essendo gli archi non orientati
             for j in range(i+1, self.num_nodes):
-                self.num_edges += 1
+                self.num_edges += 1     # aumento il numero di archi
                 self.matr_adj[i][j] = self.calculateWeight(self.coordinates[i], self.coordinates[j])
                 self.matr_adj[j][i] = self.matr_adj[i][j]
 
-
-
-
-
     def printG(self):
         print("Il grafo", self.name, "ha ", self.num_nodes, "nodi e ", self.num_edges, "archi")
-
         print(self.matr_adj)
 
-
     def calculateWeight(self, coordinates1, coordinates2):
-        if self.coord_type != "GEO\n":
+        """
+        :param coordinates1: prima coppia di coordinate
+        :param coordinates2: seconda coppia di coordinate
+        :return: il peso dell'arco fra le due coppie di coordinate
+        """
+        if self.coord_type != "GEO\n":  # se sono coordinate euclidee
             x = abs(coordinates1[0] - coordinates2[0])
             y = abs(coordinates1[1] - coordinates2[1])
             return round((x**2 + y**2)**0.5)
-        else:
+        else:                           # se sono coordinate geografiche
             RRR = 6378.388
 
             q1 = math.cos(coordinates1[1] - coordinates2[1])
@@ -55,128 +54,102 @@ class Graph:
             q3 = math.cos(coordinates1[0] + coordinates2[0])
             return int(RRR * math.acos(0.5 * ((1.0 + q1) * q2 - (1.0 - q1) * q3)) + 1.0)
 
-    def hkVisit(self, v, subset_nodes, distances, previous, start_time, stop, mymap):
+    def hkVisit(self, v, subset_nodes, distances, previous, start_time, stop, my_map):   # todo Da commentare
         """
         :param v: nodo destinazione
         :param subset_nodes: sottoinsieme di nodi in cui viene calcolato il peso del cammino minimo
+        :param distances: todo da completare
+        :param previous: todo da completare
+        :param start_time: tempo di inizio
+        :param stop: tempo di fine del timer
+        :param my_map: todo da completare
         :return: peso del cammino minimo da 0 a v che visita tutti i vertici in subset_nodes
         """
-        #print("HK_VISIT", v, subset_nodes)
         g = tuple(subset_nodes)
 
         if len(subset_nodes) == 1 and subset_nodes[0] == v:
             return self.matr_adj[0][v], distances, previous, stop
 
         elif (v, g) in distances:
-            #print("ENTRATO!",distances[v][g])
             return distances[v, g], distances, previous, stop
         else:
             min_dist = sys.maxsize
             min_prec = -1
             subset = copy.deepcopy(subset_nodes)
             subset.remove(v)
-
             for vertex in subset_nodes:
                 if stop:
                     break
 
                 if vertex != v:
-                    dist, distances, previous, stop = self.hkVisit(vertex, subset, distances, previous, start_time, stop, mymap)
-
+                    dist, distances, previous, stop = self.hkVisit(vertex, subset, distances, previous, start_time, stop, my_map)
                     if dist + self.matr_adj[vertex][v] < min_dist:
                         min_dist = dist + self.matr_adj[vertex][v]
                         min_prec = vertex
+
             if time.time() - start_time > 20*60:
                 stop = True
                 return min_dist, distances, previous, stop
 
-            # distances[0][tuple(subset_nodes)] = 10
-
             distances.update({(v, g): min_dist})
-            #print(v, g)
-            #print(distances[v,g])
-
-            #distances[v][tuple(subset_nodes)] = min_dist
-
             return min_dist, distances, previous, stop
 
-
-
     def hkTsp(self, start_time):
-        column = []  # lista di Distance in cui ogni elemento è un DistanceItem con valore la distanza
-        mymap = {}
-        counter = 0
-        """
-        for i in range(self.num_nodes):
-            column_copia = copy.deepcopy(column)
-            #print(column_copia)
-            column.append([i])
-            mymap[tuple([i])] = counter
-            counter += 1
-            for j in range(0, len(column_copia)):
-                #print(i)
-                column_copia[j].append(i)
-                #print(column_copia[j])
-                mymap[tuple(column_copia[j])] = counter
-                column.append(column_copia[j])
-                counter += 1
-        #print(column)
-        distances = np.full((self.num_nodes, len(column)), -1, dtype=float)
-        """
+        my_map = {}
         distances = {}
         previous = [-1 for x in range(self.num_nodes)]           # lista di Distance in cui ogni elemento è un DistanceItem con valore dell'id del predecessore
         vertices = [x for x in range(self.num_nodes)]
-        return self.hkVisit(0, vertices, distances, previous, start_time, False, mymap)
-
+        return self.hkVisit(0, vertices, distances, previous, start_time, False, my_map)
 
     def nearestNeighbor(self):
-        circuit = []
-        total_circuit_length = 0
-        circuit.append(0)
-        visited_nodes = [False for x in range(self.num_nodes)]
-        visited_nodes[0] = True
-        for i in range(1, self.num_nodes):
+        circuit = []        # inizializzo il circuito vuoto
+        total_circuit_length = 0        # inizializzo la lunghezza del circuito a 0
+        visited_nodes = [False for x in range(self.num_nodes)]  # inizializzo un array che per ciascun nodo tiene traccia se è stato aggiunto al circuito o no
+        circuit.append(0)       # aggiungo il nodo iniziale al circuito
+        visited_nodes[0] = True             # ho aggiunto il nodo iniziale al circuito
+
+        for i in range(1, self.num_nodes):  # per ogni altro nodo
             min_dist = sys.maxsize
-            choosen_index = -1
-            for index, val in enumerate(self.matr_adj[circuit[-1]]):
+            choosen_index = -1          # inizializzo l'indice del nodo scelto
+
+            for index, val in enumerate(self.matr_adj[circuit[-1]]):    # scelgo il nodo che ha minima distanza
                 if val < min_dist and not visited_nodes[index]:
-                    min_dist = val
-                    choosen_index = index
+                    min_dist = val                  # salvo il peso dell'arco
+                    choosen_index = index               # salvo l'indice del nodo
 
-            circuit.append(choosen_index)
+            circuit.append(choosen_index)       # aggiungo il nuovo nodo al circuito
             visited_nodes[choosen_index] = True
-            total_circuit_length += min_dist
+            total_circuit_length += min_dist    # aggiorno il valore della lunghezza del circuito
 
-        total_circuit_length += self.matr_adj[0][circuit[-1]]       #aggiungo la distanza fra l'ultimo nodo trovato e il nodo sorgente
-        circuit.append(0)
-        return circuit, total_circuit_length        # Todo Togliere il ritorno di circuit che non serve
+        total_circuit_length += self.matr_adj[0][circuit[-1]]       # aggiungo la distanza fra l'ultimo nodo trovato e il nodo sorgente
+        circuit.append(0)               # aggiungo il nodo iniziale anche alla fine del circuito in modo da creare un vero e proprio circuito
+        return total_circuit_length
 
-    def createArraysOfEdges(self):      # Controllato, ritorna un array di due dimensioni in cui ciascuna riga è una tupla di 3 valori ordinata secondo il primo valore
+    def createArraysOfEdges(self):
+        """
+        :return res: un array di due dimensioni in cui ciascuna riga è una tupla di 3 valori ordinata secondo il peso minore
+                 del tipo: (peso, indice i, indice j)
+        """
         res = [(self.matr_adj[i][j], i, j)
                for i in range(self.num_nodes - 1)
                for j in range(i+1, self.num_nodes)]
 
         res = sorted(res, key=lambda t: t[0])
-
         return res
 
     def kruskalMST(self):
-        couples = []
+        couples = []    # array di nodi
         for i in range(self.num_nodes):
-            couples.append(Node(i))
-        #arr_sets = []
-        set_tree = Tree(self.num_nodes)
+            couples.append(Node(i))     # aggiunge n nodi a couples
+        set_tree = Tree(self.num_nodes)     # aggiunge n nodi all'albero
         for i in range(self.num_nodes):
-            set_tree.makeSet(i)
-        edges = self.createArraysOfEdges()  # todo crea la funzione che crea gli archi ordinati di peso crescente
-        #print("uscito")
-        for val in edges:
-            #print(val[0])
-            if set_tree.findSet(val[1]) != set_tree.findSet(val[2]):
-                #print("entrato")
-                couples[val[1]].addEdgeToNode(Edge(val[1], val[2], val[0]))
-                couples[val[2]].addEdgeToNode(Edge(val[2], val[1], val[0]))
-                set_tree.union(val[1], val[2])
+            set_tree.makeSet(i)         #crea n insiemi disgiunti
+        edges = self.createArraysOfEdges()  # ordina gli archi in peso crescente
+        for val in edges:       # per ogni arco ordinato secondo peso crescente
+            if set_tree.findSet(val[1]) != set_tree.findSet(val[2]):    # se i nodi non fanno parte dello stesso albero
+                couples[val[1]].addEdgeToNode(Edge(val[1], val[2], val[0]))     # aggiungo un arco fra il primo ed il secondo nodo
+                couples[val[2]].addEdgeToNode(Edge(val[2], val[1], val[0]))     # aggiungo un arco fra il secondo ed il primo nodo
+                set_tree.union(val[1], val[2])              # faccio l'unione degli alberi
 
         already_inserted = [False for x in range(self.num_nodes)]
         dictionary = {}
@@ -192,10 +165,9 @@ class Graph:
         :param couples: array di nodi con la lista di adiacenza per ciascun nodo
         :param counter: contatore per la numerazione dei nodi
         :param dictionary: dizionario che associa la numerazione dei nodi all'id del nodo nel grafo originale(self)
-        :param start_node: nodo di partenzaarr_sets = []
-        :return: dict finale
+        :param start_node: nodo di partenza
+        :return dictionary: dizionario finale
         """
-        #print("deptFirstSearch")
         dictionary[counter] = start_node.id
         already_inserted[start_node.id] = True
         counter += 1
@@ -208,7 +180,6 @@ class Graph:
 
     def calculate_distance_MST(self, graph_enumeration):
         dist = 0
-        #print("calculate_distance_MST")
         for i in range(self.num_nodes):
             dist += self.matr_adj[graph_enumeration[i]][graph_enumeration[i+1]]
         return dist
--- Lab3/Main.py
+++ Lab3/Main.py
@@ -1,22 +1,20 @@
-import numpy as np
 import codecs
 from Graph import Graph
 import math
 import time
-import sys
+
 
 def main():
-    path_info_array = ["./Files/burma14.tsp", "./Files/d493.tsp", "./Files/dsj1000.tsp", "./Files/eil51.tsp", "./Files/gr229.tsp", "./Files/kroD100.tsp", "./Files/ulysses22.tsp"]
-
-    #path_info = ["./Files/piccolo_esempio.tsp"]
+    path_info_array = ["./Files/burma14.tsp", "./Files/d493.tsp", "./Files/dsj1000.tsp", "./Files/eil51.tsp",
+                       "./Files/gr229.tsp", "./Files/kroD100.tsp", "./Files/ulysses22.tsp"]
     graph_list = []
-    #path_info = ["./Files/ulysses22.tsp"]
+    #path_info = ["./Files/burma14.tsp"]
 
-    for i in range(len(path_info_array)):
+    for i in range(len(path_info_array)):       # per ciascun file
         name = ""       # nome del grafo
         dimension = -1      # numero dei nodi
         coord_type = ""     # tipo di coordinate
-        coordinates = []    # lista di tuple della forma (coordinata1, coordinata2)
+        coordinates = []    # lista di tuple della forma (coordinataX, coordinataY)
         next_are_coords = False     # se è True significa che sto parsando le coordinate
         line = ""       # inizializzazione della linea che verrà utilizzata per scorrere il file
 
@@ -31,8 +29,10 @@ def main():
 
                     if a[0] == "DIMENSION" or a[0] == "DIMENSION ":
                         dimension = int(a[1].replace(" ", ""))     # salvo la dimensione dei nodi del grafo
+
                     if a[0] == "EDGE_WEIGHT_TYPE" or a[0] == "EDGE_WEIGHT_TYPE ":
                         coord_type = a[1].replace(" ", "")   # salvo il tipo di coordinate
+
                     if a[0] == "NODE_COORD_SECTION\n":
                         next_are_coords = True      # le prossime righe del file saranno coordinate
 
@@ -40,44 +40,39 @@ def main():
                     a = line.split(" ")     # divido la linea rispetto agli spazi
                     a = [i for i in a if i != ""]
                     if coord_type == "EUC_2D\n":      # se sono euclidee
-                        #print("STAMPO", a[1])
                         coordinates.append((float(a[1]), float(a[2][:-1])))      # float gestisce la codifica numerica esponenziale
 
                     else:
-                        deg = int(float(a[1]))
-                        #print(deg)
+                        deg = int(float(a[1]))      # tronca all'intero
                         min = float(a[1]) - deg
-                        radX = math.pi * (deg + 5.0 * min / 3.0) / 180.0
+                        rad_x= math.pi * (deg + 5.0 * min / 3.0) / 180.0    # calcolo coordinata x
 
                         deg = int(float(a[2][:-1]))
                         min = float(a[2][:-1]) - deg
-                        radY = math.pi * (deg + 5.0 * min / 3.0) / 180.0
+                        rad_y = math.pi * (deg + 5.0 * min / 3.0) / 180.0    # calcolo coordinata y
 
-                        coordinates.append((radX, radY))    # aggiungo le coordinate alla lista
+                        coordinates.append((rad_x, rad_y))    # aggiungo le coordinate alla lista
 
-                line = f.readline()     # passo alla linea successiva
-        graph_list.append(Graph(name, dimension, coord_type, coordinates))
+                line = f.readline()     # leggo la linea successiva
+        graph_list.append(Graph(name, dimension, coord_type, coordinates))          # aggiungo alla lista di grafi il grafo creato
 
-    for graph in graph_list:
+    for graph in graph_list:        # per ciascun grafo
         print(graph.name)
-        start_time = time.time()
-        min_dist, distances, previous, stop = graph.hkTsp(start_time)
-        print("Minima distanza algoritmo esatto = ", min_dist)
-        print("tempo algoritmo esatto =", time.time() - start_time)
+        start_time = time.time()        # faccio partire il timer
+        min_dist, distances, previous, stop = graph.hkTsp(start_time)   # eseguo Held-Karp
+        print("Minima distanza algoritmo esatto = ", min_dist)      # stampo il cammino minimo trovato
+        print("tempo algoritmo esatto =", time.time() - start_time)     # stampo il tempo impiegato
 
-        start_time = time.time()
-        circuit, min_dist = graph.nearestNeighbor()
-        print("Minima distanza euristica nearestNeighbor = ", min_dist)
-        print("tempo nearestNeighbor =", time.time() - start_time)
+        start_time = time.time()        # faccio partire il timer
+        min_dist = graph.nearestNeighbor()         # eseguo l'euristica Nearest Neighbor
+        print("Minima distanza euristica Nearest Neighbor = ", min_dist)    # stampo il cammino minimo trovato
+        print("tempo Nearest Neighbor =", time.time() - start_time)     # stampo il tempo impiegato
 
-        start_time = time.time()
-        dist = graph.kruskalMST()
-        print("Minima distanza 2 approssimata = ", dist)
-        print("tempo 2 approssimata =", time.time() - start_time)
-
-
-
+        start_time = time.time()        # faccio partire il timer
+        dist = graph.kruskalMST()       # eseguo l'euristica 2-approssimata
+        print("Minima distanza 2 approssimata = ", dist)    # stampo il cammino minimo trovato
+        print("tempo euristica 2-approssimata = ", time.time() - start_time)    # stampo il tempo impiegato
 
 
 if __name__ == '__main__':
-        main()
+    main()
